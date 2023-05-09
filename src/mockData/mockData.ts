dfdcfb43adf76360fe2439d751607a8f3abfa41d
Nicolas Vera <nicolasveraa97@gmail.com>
2023-05-09 18:12:07 -0300

    Added path de las fotos a los mocks
    Le agregue el path de las fotos a los mocks de los posts para ir implementandolas

--- src/mockData/mockData.ts
+++ src/mockData/mockData.ts
@@ -9,6 +9,7 @@ export default {
                 "creado": "2023-05-05",
                 "conclusion": "este cafe estaba rico",
                 "lugar": "Kersen",
+                "foto":'./assets/coffee1.jpg', // a chequear este path asjasjasj
                 "createdAt": "2023-05-05T20:34:19.726Z",
                 "updatedAt": "2023-05-05T20:49:27.816Z",
                 "publishedAt": "2023-05-05T20:34:36.144Z",
@@ -23,6 +24,7 @@ export default {
                 "descripcion": "AAAAAAAAAA",
                 "creado": "2023-05-18",
                 "conclusion": "XSSSSS",
+                "foto":'./assets/coffee2.jpg',
                 "lugar": "Kersen",
                 "createdAt": "2023-05-05T20:47:29.756Z",
                 "updatedAt": "2023-05-05T20:47:36.097Z",
@@ -39,6 +41,7 @@ export default {
                 "creado": "2023-05-06",
                 "conclusion": "mmmmmmm",
                 "lugar": "Kersen",
+                "foto":'./assets/coffee3.jpg',
                 "createdAt": "2023-05-06T18:25:18.965Z",
                 "updatedAt": "2023-05-06T18:25:22.762Z",
                 "publishedAt": "2023-05-06T18:25:22.759Z",
