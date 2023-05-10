c7b166a44fdb7e4a16bc671f302f4fd031e838e8
Nicolas Vera <nicolasveraa97@gmail.com>
2023-05-10 14:09:38 -0300

    Fix: size de foto y un toque de margin
    Las fotos no entraban del todo bien en su caja asique se les redujo el tamaño levemente. Tambien se corrigio el margin inferior que solo tiene que estar ahi para cuando se activa el scroll

--- src/components/BlogPost/BlogPost.tsx
+++ src/components/BlogPost/BlogPost.tsx
@@ -5,7 +5,7 @@ import styles from './BlogPost.module.css'
 export const BlogPost = ({titulo,lugar,descripcion,conclusion,creado,creadoPor, foto}) => {
   return (
     <div className={styles.blogpost}>
-        <img src={foto} height={200} width={200}/>
+        <img src={foto} height={190} width={190}/>
         <div className={styles.blogpostBody}>
           <h2>{titulo}</h2>
           <h3>Lugar: {lugar}</h3>
