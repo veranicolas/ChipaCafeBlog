59d77e6bdde99023ef8826bc3bc3c2327b81b63a
Nicolas Vera <nicolasveraa97@gmail.com>
2023-05-10 12:40:03 -0300

    Added: estilos para navbar
    Agregue estilos para los botones de los links del navbar. Una transicion cuando se le hace hover. Tambien puse un autoscroll a la homepage cuando hay muchos posts que va a quedar por el momento.

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -4,14 +4,13 @@ const Navbar = () => {
     return (
         <div className={styles.navbarComponent}>
             <div className={styles.customLink}>
-            <a href="/cafe">Cafe</a>
-            </div>
-            
-            <div className={styles.customLink}>
-            <a href="/chipa">Chipa</a>
+                <a href="/cafe">Cafe</a>
             </div>
             <div className={styles.customLink}>
-            <a href="/nosotros">Nosotros</a>
+                <a href="/chipa">Chipa</a>
+            </div>
+            <div className={styles.customLink}>
+                <a href="/nosotros">Nosotros</a>
             </div>
         </div>
     )
