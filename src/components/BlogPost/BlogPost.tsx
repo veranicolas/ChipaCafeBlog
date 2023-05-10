bc52b62af0a2326d45e9d72f2a7261d48a5c3758
Nicolas Vera <nicolasveraa97@gmail.com>
2023-05-10 14:05:46 -0300

    Added: estilos homepage y blogpost
    Agregados estilos momentaneos para ver las cards de los posts

--- src/components/BlogPost/BlogPost.tsx
+++ src/components/BlogPost/BlogPost.tsx
@@ -2,14 +2,17 @@
 //@ts-nocheck
 import React from "react"
 import styles from './BlogPost.module.css'
-export const BlogPost = ({titulo,lugar,descripcion,conclusion,creado,creadoPor}) => {
+export const BlogPost = ({titulo,lugar,descripcion,conclusion,creado,creadoPor, foto}) => {
   return (
     <div className={styles.blogpost}>
-        <h2>{titulo}</h2>
-        <h3>Lugar: {lugar}</h3>
-        <p>{descripcion}</p>
-        <p>Conclusion: {conclusion}</p>
-        <p>{creadoPor}, {creado}.</p>
+        <img src={foto} height={200} width={200}/>
+        <div className={styles.blogpostBody}>
+          <h2>{titulo}</h2>
+          <h3>Lugar: {lugar}</h3>
+          <p style={{padding: '0 20px'}}>{descripcion}</p>
+          <p className={styles.conclusion}>Conclusion: {conclusion}</p>
+          <p>{creadoPor}, {creado}.</p>
+        </div>
     </div>
   )
 }
