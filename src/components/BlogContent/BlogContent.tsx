553195d8e7616c662188d36d9296d0cad02cec70
Nicolas Vera <nicolasveraa97@gmail.com>
2023-05-11 04:32:25 -0300

    Added: animaciones a los blogs
    Se agregaron animaciones a la primera renderizacion de los blogs en la homepage, y otra transicion para cuando se les haga hover que se expandan

--- src/components/BlogContent/BlogContent.tsx
+++ src/components/BlogContent/BlogContent.tsx
@@ -1,26 +1,50 @@
-//@ts-nocheck
-import React from 'react';
-
 import { BlogPost } from '../BlogPost/BlogPost';
 import postData from '../../mockData/mockData.ts';
+import { animated, useSpring } from '@react-spring/web'
 
 import styles from './BlogContent.module.css'
+import { useEffect } from 'react';
 
 export const BlogContent = () => {
+  
+  const [springs, api] = useSpring(()=>({
+    from:{
+      opacity:0
+    },
+    config:{
+      duration:600
+    }
+  }))
+
+  useEffect(()=>{
+    api.start({
+      from:{
+        opacity:0
+      },
+      to:{
+        opacity:1
+      }
+    })
+  },[])
+  
   return (
-    <div className={styles.blogpostContainer}>
-        {postData.data.map((post) => (
-            <BlogPost
-                key={post.id}
-                titulo={post.attributes.titulo}
-                lugar={post.attributes.lugar}
-                descripcion={post.attributes.descripcion}
-                conclusion={post.attributes.conclusion}
-                creado={post.attributes.creado}
-                creadoPor={post.attributes.creadoPor}
-                foto={post.attributes.foto}
-            />
-        ))}
-    </div>
+    <animated.div 
+      style={{
+        ...springs
+      }}
+      className={styles.blogpostContainer}>
+      {postData.data.map((post) => (
+          <BlogPost
+              key={post.id}
+              titulo={post.attributes.titulo}
+              lugar={post.attributes.lugar}
+              descripcion={post.attributes.descripcion}
+              conclusion={post.attributes.conclusion}
+              creado={post.attributes.creado}
+              creadoPor={post.attributes.creadoPor}
+              foto={post.attributes.foto}
+          />
+      ))}
+    </animated.div>
   )
 }
--- src/components/BlogPost/BlogPost.tsx
+++ src/components/BlogPost/BlogPost.tsx
@@ -1,18 +1,72 @@
-
-//@ts-nocheck
-import React from "react"
+import { useState } from 'react'
 import styles from './BlogPost.module.css'
-export const BlogPost = ({titulo,lugar,descripcion,conclusion,creado,creadoPor, foto}) => {
+
+import { animated , useSpring } from '@react-spring/web'
+
+interface BlogPostProps {
+  titulo:string,
+  lugar:string,
+  descripcion:string,
+  conclusion:string,
+  creado:string,
+  creadoPor:string,
+  foto:string,
+}
+
+export const BlogPost = ({titulo,lugar,descripcion,conclusion,creado,creadoPor, foto}:BlogPostProps) => {
+
+  const [expanded, setExpanded] = useState(false)
+  
+  const [springs, api] = useSpring(()=>({
+    from:{
+      height: 200
+    }
+  }))
+
+  const handleMouseEnter = () =>{
+    api.start({
+      from:{
+        height:200
+      },
+      to:{
+        height:400
+      }
+    })
+
+    setExpanded(true)
+  }
+
+  const handleMouseLeave = ()=>{
+    api.start({
+      from:{
+        height:400
+      },
+      to:{
+        height:200
+      }
+    })
+
+    setExpanded(false)
+  }
+  
   return (
-    <div className={styles.blogpost}>
+    <animated.div
+        style={
+          {
+            ...springs
+          }}
+        className={styles.blogpost}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+    >
         <img src={foto} height={190} width={190}/>
         <div className={styles.blogpostBody}>
           <h2>{titulo}</h2>
           <h3>Lugar: {lugar}</h3>
-          <p style={{padding: '0 20px'}}>{descripcion}</p>
-          <p className={styles.conclusion}>Conclusion: {conclusion}</p>
+          <p >{descripcion}</p>
+          <p style={ expanded ? {display:'block'} : {display:'none'}}>Conclusion: {conclusion}</p>
           <p>{creadoPor}, {creado}.</p>
         </div>
-    </div>
+    </animated.div>
   )
 }
