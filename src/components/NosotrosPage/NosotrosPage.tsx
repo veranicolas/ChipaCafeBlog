e80ff53670c1e554c5bd614f68c4c6287fae94ca
Nicolas Vera <nicolasveraa97@gmail.com>
2023-05-14 22:39:34 -0300

    Added: estilos a nosotros page
    Agregados los estilos basicos para la pagina con la informacion sobre los creadores e utilizadores de la pagina

--- src/components/NosotrosPage/NosotrosPage.tsx
+++ src/components/NosotrosPage/NosotrosPage.tsx
@@ -1,9 +1,27 @@
+import styles from './NosotrosPage.module.css'
+
+import fotos from './fotos'
 
 export const NosotrosPage = () => {
   return (
     <>
-    <div>NosotrosPage</div>
-    <a href="/">Home</a>
+     <div className={styles.content}>
+        <h2 style={{fontWeight:100}}>Sobre nosotros</h2>
+        <div className={styles.peopleCards}>
+          <div className={styles.people}>
+            <img src={fotos.vera} height={200} width={200}/>
+            <h3 style={{textAlign:'center'}}>Nicolas Vera</h3>
+          </div>
+          <div className={styles.people}>
+            <img src={fotos.napo} height={200} width={200}/>
+            <h3 style={{textAlign:'center'}}>Tomas Napolitano</h3>
+          </div>
+          <div className={styles.people}>
+            <img src={fotos.leo} height={200} width={200}/>
+            <h3 style={{textAlign:'center'}}>Leo Lalanne</h3>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
