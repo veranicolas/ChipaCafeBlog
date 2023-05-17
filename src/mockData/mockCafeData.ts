d9435e558e8c0a936e6a13c48edbe01b4d47d26f
Nicolas Vera <nicolasveraa97@gmail.com>
2023-05-17 16:09:28 -0300

    Added: mas informacion a los mocks
    Se agrego mas informacion y mas atributos a los mocks de los posts, para asemejar a cuando sean de informacion real

--- src/mockData/mockCafeData.ts
+++ src/mockData/mockCafeData.ts
@@ -1,51 +1,57 @@
 export default {
     "data": [
         {
-            "id": 1,
+            "id": 4,
             "attributes": {
-                "titulo": "Un lugar icónico en París",
-                "descripcion": "Café de Flore ha estado operando desde 1887. Este café es conocido por su ambiente bohemio y su historia rica en la cultura francesa. ",
-                "creado": "2023-05-05",
-                "conclusion": "El café ha sido visitado por grandes escritores y artistas, como Ernest Hemingway y Pablo Picasso. Además de la decoración tradicional del café, la bebida es excelente. Sus opciones de café son impresionantes y el servicio es excepcional. También ofrecen una amplia variedad de platos ligeros y pasteles. En general, este lugar es una excelente opción para experimentar el auténtico café francés y sumergirse en la rica historia y cultura de París.",
+                "titulo": "Un Viaje en el Tiempo a la Bohemia Parisina",
+                "descripcion": "El Café de Flore, ubicado en el corazón de París, es un verdadero tesoro para los amantes del café y los entusiastas de la cultura.",
+                "creado": "2023-05-19",
+                "conclusion": "En resumen, el Café de Flore es un lugar icónico que no debe perderse si visitas París. Su excepcional café, su encantadora atmósfera y su historia literaria lo convierten en una experiencia única. Sumérgete en la magia de este café histórico y déjate llevar por su encanto mientras disfrutas de una taza de café que te transportará a otra época.",
                 "lugar": "Café de Flore",
+                "createdAt": "2023-05-17T18:55:34.567Z",
+                "updatedAt": "2023-05-17T19:04:42.898Z",
+                "publishedAt": "2023-05-17T18:55:36.838Z",
+                "creadoPor": "Leo Lallane",
                 "foto":'../coffee1.jpg',
-                "createdAt": "2023-05-05T20:34:19.726Z",
-                "updatedAt": "2023-05-05T20:49:27.816Z",
-                "publishedAt": "2023-05-05T20:34:36.144Z",
-                "creadoPor": "Leo Lallane",
-                "fechaPublicar": "2023-05-31"
+                "fechaPublicar": "2023-05-31",
+                "cuerpo1": "Una de las mejores cosas del Café de Flore es su excelente café. Desde el primer sorbo, te das cuenta de que estás degustando una taza de calidad excepcional. La variedad de opciones disponibles es impresionante, desde un clásico espresso hasta un cremoso café con leche. Los baristas son expertos en su oficio y se nota en cada taza que preparan.",
+                "cuerpo2": "Además de la excelente calidad del café, el Café de Flore ofrece una amplia selección de platos ligeros y deliciosos pasteles. Puedes disfrutar de una croissant recién horneada o deleitarte con una ensalada fresca mientras disfrutas de tu bebida. El servicio es impecable y el personal amable y atento, lo que contribuye a una experiencia agradable en general."
             }
         },
         {
-            "id": 2,
+            "id": 5,
             "attributes": {
-                "titulo": "Es un lugar moderno",
-                "descripcion": "Blue Bottle Coffee es elegante para disfrutar del café en la ciudad de Nueva York.",
-                "creado": "2023-05-18",
-                "conclusion": "Su café de especialidad es de alta calidad y están comprometidos con el suministro de granos de café éticamente obtenidos y de comercio justo. El servicio es rápido y eficiente, lo que hace que sea una excelente opción para una parada rápida antes de dirigirse al trabajo. Además, su selección de pasteles y bocadillos es deliciosa y se complementa perfectamente con su café. Si estás buscando un lugar para disfrutar de un café de alta calidad en un ambiente moderno, Blue Bottle Coffee es una excelente opción.",
-                "foto":'../coffee2.jpg',
+                "titulo": "Un Oasis Moderno para los Amantes del Café",
+                "descripcion": "Blue Bottle Coffee es un lugar moderno y elegante ubicado en el corazón de la ciudad de Nueva York. ",
+                "creado": "2023-05-20",
+                "conclusion": "En resumen, Blue Bottle Coffee es un oasis moderno para los amantes del café en la ciudad de Nueva York. Desde la calidad excepcional de su café hasta su ambiente elegante y su compromiso con la sostenibilidad, este lugar es perfecto para aquellos que buscan una experiencia de café única y memorable. ¡No te pierdas la oportunidad de descubrir el sabor y el encanto de Blue Bottle Coffee!",
                 "lugar": "Blue Bottle Coffee",
-                "createdAt": "2023-05-05T20:47:29.756Z",
-                "updatedAt": "2023-05-05T20:47:36.097Z",
-                "publishedAt": "2023-05-05T20:47:36.094Z",
+                "createdAt": "2023-05-17T18:57:42.487Z",
+                "updatedAt": "2023-05-17T19:04:49.378Z",
+                "publishedAt": "2023-05-17T18:58:40.255Z",
                 "creadoPor": "Leo Lallane",
-                "fechaPublicar": null
+                "fechaPublicar": "2023-05-26",
+                "foto":'../coffee2.jpg',
+                "cuerpo1": "Lo primero que llama la atención en Blue Bottle Coffee es la calidad excepcional de su café. Con un enfoque en los granos de café de especialidad cuidadosamente seleccionados, cada taza es una obra maestra en sí misma. Ya sea que prefieras un café expreso equilibrado y suave o un cappuccino adornado con elaboradas figuras de arte latte, el café aquí está perfectamente preparado y lleno de sabores ricos y complejos.",
+                "cuerpo2": "Además de su café excepcional, Blue Bottle Coffee se destaca por su servicio atento y amable. Los baristas están dedicados a su oficio y te guiarán a través de su amplia selección de opciones, ofreciendo recomendaciones personalizadas según tus preferencias. El ambiente moderno y minimalista del lugar crea un espacio tranquilo para disfrutar de tu bebida y relajarte en medio del bullicio de la ciudad."
             }
         },
         {
-            "id": 3,
+            "id": 6,
             "attributes": {
-                "titulo": "Es para disfrutar del café en Hong Kong.",
-                "descripcion": "Con un ambiente moderno y contemporáneo, este lugar es conocido por su café de alta calidad y su amplia variedad de opciones de café de especialidad. ",
-                "creado": "2023-05-06",
-                "conclusion": " Además, su selección de comida es excepcional, con opciones para todos los gustos, desde comidas ligeras hasta platos completos. El servicio es rápido y eficiente, lo que lo convierte en una excelente opción para una reunión rápida o para una larga sesión de estudio o trabajo. En general, The Coffee Academics es un lugar imprescindible para cualquier amante del café que visite Hong Kong.",
+                "titulo": "Un Destino de Café Excepcional en Hong Kong",
+                "descripcion": "The Coffee Academics es un lugar único y apasionante para los amantes del café en la vibrante ciudad de Hong Kong.",
+                "creado": "2023-05-18",
+                "conclusion": "En resumen, The Coffee Academics es un destino de café excepcional en Hong Kong. Con su café de alta calidad, su deliciosa comida, su servicio amable y su enfoque en la educación, este lugar ofrece una experiencia completa y gratificante para los entusiastas del café. Si estás en Hong Kong y buscas una experiencia única de café, no puedes dejar de visitar The Coffee Academics.",
                 "lugar": "The Coffee Academics",
-                "foto":'../coffee3.jpg',
-                "createdAt": "2023-05-06T18:25:18.965Z",
-                "updatedAt": "2023-05-06T18:25:22.762Z",
-                "publishedAt": "2023-05-06T18:25:22.759Z",
+                "createdAt": "2023-05-17T18:59:58.568Z",
+                "updatedAt": "2023-05-17T19:04:33.727Z",
+                "publishedAt": "2023-05-17T19:00:00.896Z",
                 "creadoPor": "Leo Lallane",
-                "fechaPublicar": "2023-05-26"
+                "fechaPublicar": "2023-05-20",
+                "foto":'../coffee3.jpg',
+                "cuerpo1": "La calidad del café en The Coffee Academics es simplemente excepcional. Con una dedicación a los granos de café de la más alta calidad y un enfoque en la artesanía del café, cada taza que sirven es una experiencia sensorial única. Ya sea que elijas un espresso audaz y vigorizante o una taza de café filtrado con delicadeza, cada sorbo es una explosión de sabores distintos y complejos que deleitará tu paladar.",
+                "cuerpo2": "Pero The Coffee Academics va más allá del café excepcional. Su menú también ofrece una amplia selección de platos deliciosos y creativos que complementan perfectamente las bebidas. Desde opciones ligeras y saludables hasta comidas más sustanciales, hay algo para todos los gustos y preferencias. Además, el servicio es amable y atento, lo que te hace sentir bienvenido y cuidado durante toda tu visita."
             }
         }
     ],
@@ -58,4 +64,3 @@ export default {
         }
     }
 }
-
--- src/mockData/mockChipaData.ts
+++ src/mockData/mockChipaData.ts
@@ -3,33 +3,37 @@ export default {
         {
             "id": 1,
             "attributes": {
-                "titulo": "El ambiente es agradable y acogedor",
+                "titulo": "Donde la Tradición y el Sabor se Encuentran",
                 "createdAt": "2023-05-13T19:34:39.690Z",
-                "updatedAt": "2023-05-13T19:34:41.749Z",
+                "updatedAt": "2023-05-17T19:04:20.636Z",
                 "publishedAt": "2023-05-13T19:34:41.746Z",
-                "descripcion": "Este local es famoso por sus deliciosos chipas hechos en horno de barro, con ingredientes frescos y de alta calidad.",
+                "descripcion": "Desde el momento en que pones un pie en este acogedor establecimiento, te envuelve el aroma irresistible de la chipa recién horneada, despertando tus sentidos y abriendo tu apetito.",
                 "creado": "2023-05-09",
-                "conclusion": "Los chipas son suaves, masticables y con un sabor a queso y leña inigualable. También ofrecen diferentes variedades, como el chipa guazú, que es más grande y se sirve como una especie de torta. El ambiente es agradable y acogedor, y el personal es amable y atento.",
-                "foto":'../chipa1.jpg',
+                "conclusion": "En resumen, La Casa del Chipa es el lugar perfecto para aquellos que buscan sabores auténticos y tradicionales. Sus chipas recién horneadas, su variedad de opciones y su servicio amable te transportarán a la esencia misma de esta delicia paraguaya. Si deseas probar la auténtica chipa en un ambiente acogedor, no puedes dejar de visitar La Casa del Chipa.",
                 "lugar": "La Casa del Chipa",
                 "creadoPor": "Nicolas Vera",
-                "fechaPublicar": "2023-05-06"
+                "fechaPublicar": "2023-05-06",
+                "foto":'../chipa1.jpg',
+                "cuerpo1": "Lo que hace que La Casa del Chipa sea especial es su dedicación a preservar la autenticidad de esta delicia tradicional. Cada chipa es preparada con recetas que han sido transmitidas de generación en generación, manteniendo viva la esencia de esta joya culinaria. La masa es perfectamente mezclada y amasada, logrando una textura suave y un sabor equilibrado. El relleno de queso fresco se derrite en tu boca, aportando un toque cremoso y delicioso a cada bocado.",
+                "cuerpo2": "La variedad de chipas en La Casa del Chipa es asombrosa. Desde las clásicas chipas tradicionales hasta las versiones con agregados como jamón, salame o aceitunas, hay una opción para cada gusto. Además, la frescura de sus productos es evidente, ya que cada chipa es horneada diariamente, asegurando que disfrutes de la mejor calidad en cada visita."
             }
         },
         {
             "id": 2,
             "attributes": {
-                "titulo": "Agradable y acogedor",
+                "titulo": "Donde los Sabores Tradicionales Cobran Vida",
                 "createdAt": "2023-05-13T19:35:39.231Z",
-                "updatedAt": "2023-05-13T19:35:41.020Z",
+                "updatedAt": "2023-05-17T19:02:11.564Z",
                 "publishedAt": "2023-05-13T19:35:41.018Z",
-                "descripcion": "La Rosita es una panadería tradicional que ofrece una gran variedad de panes y pasteles, pero su especialidad es el chipa.",
-                "foto":'../chipa2.jpg',
+                "descripcion": "Panadería La Rosita es un encantador rincón culinario ubicado en el corazón de la ciudad. ",
                 "creado": "2023-05-25",
-                "conclusion": "Su chipa es suave y masticable, con un sabor intenso a queso y mandioca. También ofrecen diferentes variedades, como el chipa relleno de jamón y queso. El ambiente es agradable y acogedor, y el personal es amable y atento.\n\n",
+                "conclusion": "\nEn resumen, Panadería La Rosita es un tesoro culinario que no te puedes perder. Desde sus panes artesanales hasta sus dulces y pasteles excepcionales, cada visita a esta panadería es un verdadero placer para los sentidos. Si buscas sabores tradicionales y auténticos en un ambiente acogedor, no puedes dejar de visitar Panadería La Rosita.\n",
                 "lugar": "Panadería La Rosita",
                 "creadoPor": "Nicolas Vera",
-                "fechaPublicar": "2023-05-31"
+                "fechaPublicar": "2023-05-31",
+                "foto":'../chipa2.jpg',
+                "cuerpo1": "Lo que distingue a Panadería La Rosita es su dedicación a la autenticidad y a la calidad en cada uno de sus productos. Desde los panes artesanales hasta los pasteles y las galletas, cada creación es elaborada con maestría y con ingredientes frescos y de primera calidad. Cada bocado es una explosión de sabores auténticos y familiares que te transportan a tus recuerdos más queridos.",
+                "cuerpo2": "Su chipa es suave y masticable, con un sabor intenso a queso y mandioca. También ofrecen diferentes variedades, como el chipa relleno de jamón y queso. El ambiente es agradable y acogedor, y el personal es amable y atento."
             }
         }
     ],
@@ -42,3 +46,4 @@ export default {
         }
     }
 }
+
