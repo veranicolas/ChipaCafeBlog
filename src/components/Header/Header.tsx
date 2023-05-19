978953989ce841b7cefdc9600a5dae884aeb4355
Nicolas Vera <nicolasveraa97@gmail.com>
2023-05-19 05:22:19 -0300

    Added: menu para mobile
    Cuando el breakpoint es menor a 768px, el menu hamburguesa abre el menu responsive ocupando la pantalla completa

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,44 @@
+import { useEffect, useState } from 'react';
+import { animated, useSpring } from '@react-spring/web';
+
+import { Link } from 'react-router-dom';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import styles from './Header.module.css'
 import Navbar from './Navbar';
-import { Link } from 'react-router-dom';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useEffect, useState } from 'react';
 
-const HamburguerMenu = () =>{
+const MobileNavbarMenu = ({isOpen, handleMenu}:any) =>{
+
+    const displayAnimation = useSpring({
+        opacity: 1, 
+        from: { opacity: 0 }, 
+        config: { duration: 500 }
+    });
+
+    const hideAnimation = useSpring({
+        opacity: 0, 
+        from: { opacity: 1 }, 
+        config: { duration: 500 }
+    });
+
+    return(
+        <animated.div style={isOpen ? {...displayAnimation} : {...hideAnimation}} className={styles.mobileNavbar}>
+            <div className={styles.mobileNavbarMenu}>
+                <FontAwesomeIcon icon={faXmark} style={{color: "#f3f4f7",}} size='2xl' onClick={handleMenu}/>
+                <div onClick={handleMenu}>
+                    <Navbar />
+                </div>
+            </div>
+        </animated.div>
+    )
+}
+
+const HamburguerMenu = ({handleMenu}:any) =>{
 
     return(
         <div className={styles.hamburguerMenu}>
-            <FontAwesomeIcon icon={faBars} size="2xl"/>
+            <FontAwesomeIcon icon={faBars} size="2xl" onClick={handleMenu}/>
         </div>
     )
 }
@@ -17,6 +46,7 @@ const HamburguerMenu = () =>{
 const Header = () => {
 
     const [isVisible, setVisible] = useState(window.innerWidth >= 768 ? true : false)
+    const [isOpen, setOpenMenu] = useState(false)
 
     // Esto lo que hace es agregar un evento en el dom, que cuando la pantalla cambia de tamaño a mas chico que 480 que se oculte la navbar, y que si es mas grande que eso que la muestre. 
     useEffect(()=>{
@@ -37,13 +67,20 @@ const Header = () => {
         }
     }, [])
 
+    const handleMenu = () =>{
+        setOpenMenu(!isOpen)
+    }
+
     return (
         <div className={styles.headerContainer}>
             <div className={styles.title}>
                 <Link style={{textDecoration:'none', color:'black'}} to="/">CafeChipa MDP</Link>
             </div>
             {
-                isVisible ? <Navbar /> : <HamburguerMenu />
+                isVisible ? <Navbar /> : <HamburguerMenu handleMenu={handleMenu}/>
+            }
+            {
+                isOpen ? <MobileNavbarMenu isOpen={isOpen} handleMenu={handleMenu} /> : null
             }
         </div>
     );
